3858298e452fd9f6ec1548564c3ae8fdf4f6765e
Noor Mohammed <noormohammedknr2@gmail.com>
2024-03-30 12:56:58 +0530

    api folder is in progress

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+let users = [
+  { name: "Noor Mohammed", role: "Developer" },
+  { name: "Noor Mohammed", role: "Developer" },
+  { name: "Noor Mohammed", role: "Developer" },
+  { name: "Noor Mohammed", role: "Developer" },
+  { name: "Noor Mohammed", role: "Developer" },
+  { name: "Noor Mohammed", role: "Developer" },
+  { name: "Noor Mohammed", role: "Developer" },
+  { name: "Noor Mohammed", role: "Developer" },
+  { name: "Noor Mohammed", role: "Developer" },
+  { name: "Noor Mohammed", role: "Developer" },
+];
 export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+  res.status(200).json(users);
 }
